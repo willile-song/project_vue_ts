a776938532758f2171f28793246de25b8072747f
willile-song <735560190@qq.com>
2020-08-13 22:59:24 +0800

    重构了主页布局，可以通过参数获取数据，当页面样式出现问题

--- src/views/audit.vue
+++ src/views/audit.vue
@@ -1,203 +1,110 @@
 <template>
-    <div id="audit">
-        <el-tabs tab-position="left" style="height: 100%; width=159px;">
-            <el-tab-pane v-for="(item, index) in list" :key="index" :label="item.title ">
-                <!-- <search-module :title="item" /> -->
-                <div id="search-module">
-                    <h4>{{item.title}}</h4>
-                    <div class="filter">
-                        <div class="time-picter">
-                            <span class="demonstration">时间：</span>
-                            <el-date-picker
-                                v-model="valuePicter"
-                                type="datetimerange"
-                                range-separator="至"
-                                start-placeholder="开始日期"
-                                end-placeholder="结束日期"
-                            ></el-date-picker>
-                        </div>
-                        <!-- `````````````````````````````````````````````````````` -->
-                        <div class="opertion">
-                            <span>操作人：</span>
-                            <el-select
-                                class="el-select"
-                                style="width:83px;"
-                                v-model="valueOperation"
-                                filterable
-                                placeholder="请选择"
-                            >
-                                <el-option
-                                    v-for="item in options"
-                                    :key="item.value"
-                                    :label="item.label"
-                                    :value="item.value"
-                                ></el-option>
-                            </el-select>
-                        </div>
-                        <!-- ```````````````````````````````````````````````````````````` -->
-                        <div class="opertion">
-                            <span>操作：</span>
-                            <el-select
-                                class="el-select"
-                                style="width:83px;"
-                                v-model="valueOperation"
-                                filterable
-                                placeholder="请选择"
-                            >
-                                <el-option
-                                    v-for="item in options"
-                                    :key="item.value"
-                                    :label="item.label"
-                                    :value="item.value"
-                                ></el-option>
-                            </el-select>
-                        </div>
-
-                        <div class="match">
-                            <span>搜索：</span>
-                            <el-autocomplete
-                                v-model="valueMatch"
-                                :fetch-suggestions="querySearchAsync"
-                                placeholder="请输入内容"
-                                @select="handleSelect"
-                            ></el-autocomplete>
-                        </div>
-                        <el-row>
-                            <el-button type="primary">搜索</el-button>
-                            <el-button plain>重置</el-button>
-                        </el-row>
-                    </div>
-                </div>
-
-                <common-main-wrap />
-            </el-tab-pane>
-        </el-tabs>
-    </div>
+  <div id="audit">
+    <el-container>
+      <el-aside width="159px">
+        <div class="tabs">
+          <el-tabs
+            @tab-click="handleTabClick"
+            tab-position="left"
+            style="height: 100%; width=159px;"
+          >
+            <el-tab-pane v-for="(item, index) in list" :key="index" :label="item.title "></el-tab-pane>
+          </el-tabs>
+        </div>
+      </el-aside>
+      <el-main>
+        <common-main-wrap :auditType="auditType" :title="title" />
+      </el-main>
+    </el-container>
+  </div>
 </template>
 <script>
 import CommonMainWrap from "../views/common-main-wrap";
-// import SearchModule from "../components/search-module";
-class SearchObj {
-    startTime = ''
-    endStart = ''
-    operation = ''
-    operator = ''
-    valueMatch = ''
-}
+
 export default {
-    data() {
-        return {
-            // list: [
-            //     "电子签约文件",
-            //     "业务流配置",
-            //     "印章管理",
-            //     "权限管理",
-            //     "模板库管理",
-            //     "组织与成员",
-            //     "账号登录"
-            // ],
-            list: [
-                {
-                    title: "电子签约文件",
-                    auditType: 'CONTRACT', 
-                },
-                {
-                    title: "业务流配置",
-                    auditType: 'CATEGORY', 
-                },
-                {
-                    title: "印章管理",
-                    auditType: 'SEAL', 
-                },
-                {
-                    title: "权限管理",
-                    auditType: 'ROLE', 
-                },
-                {
-                    title: "模板库管理",
-                    auditType: 'TEMPLATE', 
-                },
-                {
-                    title: "组织与成员",
-                    auditType: 'EMPLOYEE', 
-                },
-                {
-                    title: "账号登录",
-                    auditType: 'ACCOUNT', 
-                },
-            ],
-            pickerOptions: {
-                shortcuts: [
-                    {
-                        text: "最近一周",
-                        onClick(picker) {
-                            const end = new Date();
-                            const start = new Date();
-                            start.setTime(
-                                start.getTime() - 3600 * 1000 * 24 * 7
-                            );
-                            picker.$emit("pick", [start, end]);
-                        }
-                    },
-                    {
-                        text: "最近一个月",
-                        onClick(picker) {
-                            const end = new Date();
-                            const start = new Date();
-                            start.setTime(
-                                start.getTime() - 3600 * 1000 * 24 * 30
-                            );
-                            picker.$emit("pick", [start, end]);
-                        }
-                    },
-                    {
-                        text: "最近三个月",
-                        onClick(picker) {
-                            const end = new Date();
-                            const start = new Date();
-                            start.setTime(
-                                start.getTime() - 3600 * 1000 * 24 * 90
-                            );
-                            picker.$emit("pick", [start, end]);
-                        }
-                    }
-                ]
-            },
-            options: [
-                {
-                    value: "选项1",
-                    label: "黄金糕"
-                }
-            ],
-            serarchData: new SearchObj()
-        };
+  data() {
+    return {
+      // list: [
+      //     "电子签约文件",
+      //     "业务流配置",
+      //     "印章管理",
+      //     "权限管理",
+      //     "模板库管理",
+      //     "组织与成员",
+      //     "账号登录"
+      // ],
+      list: [
+        {
+          title: "电子签约文件",
+          auditType: "CONTRACT",
+        },
+        {
+          title: "业务流配置",
+          auditType: "CATEGORY",
+        },
+        {
+          title: "印章管理",
+          auditType: "SEAL",
+        },
+        {
+          title: "权限管理",
+          auditType: "ROLE",
+        },
+        {
+          title: "模板库管理",
+          auditType: "TEMPLATE",
+        },
+        {
+          title: "组织与成员",
+          auditType: "EMPLOYEE",
+        },
+        {
+          title: "账号登录",
+          auditType: "ACCOUNT",
+        },
+      ],
+      // serarchData: new SearchObj(),
+      auditType: "CONTRACT",
+      title: "",
+    };
+  },
+  components: {
+    // SearchModule,
+    CommonMainWrap,
+  },
+  methods: {
+    handleTabClick(tab) {
+      console.log(tab.paneName);
+      this.auditType = this.list[tab.paneName].auditType;
+      this.title = this.list[tab.paneName].title;
+      console.log(this.auditType,this.title)
     },
-    components: {
-        // SearchModule,
-        CommonMainWrap
-    }
-    // mounted() {
-    //     console.log(this.$route.meta);
-    // }
+  },
+  // mounted() {
+  //     console.log(this.$route.meta);
+  // }
 };
 </script>
 
 
 <style scoped>
 #audit {
-    height: 100%;
-    -webkit-user-select: none;
+  height: 100%;
+  -webkit-user-select: none;
 }
 #search-module {
-    width: 100%;
-    height: 113px;
-    padding: 30px 0 30px 30px;
-    border-bottom: 1px solid rgb(228, 231, 237);
+  width: 100%;
+  height: 113px;
+  padding: 30px 0 30px 30px;
+  border-bottom: 1px solid rgb(228, 231, 237);
 }
 .filter {
-    display: flex;
-    justify-content: space-around;
-    flex-wrap: wrap;
-    padding-right: 200px;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  padding-right: 200px;
+}
+.tabs {
+  width: 160px;
 }
 </style>
--- src/views/common-main-wrap.vue
+++ src/views/common-main-wrap.vue
@@ -1,19 +1,77 @@
 <template>
-    <div id="sign-file">
-        <div>
-            <el-table class="table-list" :data="tableData" style="width: 100%">
-                <!-- <el-table-column prop="date" label="日期" width="180"></el-table-column>
-                <el-table-column prop="name" label="姓名" width="180"></el-table-column> -->
-                <el-table-column
-                    v-for="(item, index) in tableHeader"
-                    :key="index"
-                    :width="item.width"
-                    :prop="item.prop"
-                    :label="item.label"
-                ></el-table-column>
-            </el-table>
+  <div id="common-main-wrap">
+    <div id="search-module">
+      <h4>{{title}}</h4>
+      <div class="filter">
+        <div class="time-picter">
+          <span class="demonstration">时间：</span>
+          <el-date-picker
+            v-model="valuePicter"
+            type="datetimerange"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+          ></el-date-picker>
         </div>
-        <!-- <div class="pagination">
+        <div class="operator">
+          <span>操作人：</span>
+          <el-select
+            class="el-select"
+            style="width:83px;"
+            v-model="operator"
+            filterable
+            placeholder="请选择"
+          >
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </div>
+        <div class="opertion">
+          <span>操作：</span>
+          <el-select class="el-select" style="width:83px;" v-model="operation" placeholder="请选择">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </div>
+
+        <div class="match">
+          <span>搜索：</span>
+          <!-- <el-autocomplete
+                                v-model="valueMatch"
+                                placeholder="请输入内容"
+          ></el-autocomplete>-->
+          <el-autocomplete class="inline-input" v-model="keyword" placeholder="请输入内容"></el-autocomplete>
+        </div>
+        <el-row>
+          <el-button type="primary">搜索</el-button>
+          <el-button plain>重置</el-button>
+        </el-row>
+      </div>
+    </div>
+
+    <!-- // 表格部分 -->
+    <div>
+      <el-table class="table-list" :data="tableData" style="width: 100%">
+        <!-- <el-table-column prop="date" label="日期" width="180"></el-table-column>
+        <el-table-column prop="name" label="姓名" width="180"></el-table-column>-->
+        <el-table-column
+          v-for="(item, index) in tableHeader"
+          :key="index"
+          :width="item.width"
+          :prop="item.prop"
+          :label="item.label"
+        ></el-table-column>
+      </el-table>
+    </div>
+    <!-- <div class="pagination">
             <el-pagination
                 class="pagination"
                 @size-change="handleSizeChange"
@@ -23,98 +81,144 @@
                 :page-size="5"
                 layout="total, sizes, prev, pager, next, jumper"
             ></el-pagination>
-        </div> -->
-    </div>
+    </div>-->
+  </div>
 </template>
 
   <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-    props:['address'],
-    data() {
-        return {
-            listData:'',
-            tableHeader: [
-                {
-                    prop: "createTime",
-                    label: "时间",
-                    width: '',
-                },
-                {
-                    prop: "operator",
-                    label: "操作人",
-                    width: '200px',
-                },
-                {
-                    prop: "operationDesc",
-                    label: "操作",
-                    width: '',
-                },
-                {
-                    prop: "entityName",
-                    label: "操作对象",
-                    width: '',
-                },
-                {
-                    prop: "sealNames",
-                    label: "使用印章",
-                    width: '',
-                },
-                {
-                    prop: "detailedOperation", 
-                    label: "详细操作数据",
-                    width: '400px',
-                }
-            ],
-            tableData: [],
-            // currentPage4:"", 
-            paramsObj: {
+  props: ["title",'auditType'],
+  data() {
+    return {
+      listData: "",
+      tableHeader: [
+        {
+          prop: "createTime",
+          label: "时间",
+          width: "",
+        },
+        {
+          prop: "operator",
+          label: "操作人",
+          width: "200px",
+        },
+        {
+          prop: "operationDesc",
+          label: "操作",
+          width: "",
+        },
+        {
+          prop: "entityName",
+          label: "操作对象",
+          width: "",
+        },
+        {
+          prop: "sealNames",
+          label: "使用印章",
+          width: "",
+        },
+        {
+          prop: "detailedOperation",
+          label: "详细操作数据",
+          width: "400px",
+        },
+      ],
+      tableData: [],
+      // currentPage4:"",
+      valuePicter: "",
+      operator: "",
+      operation: "",
+      keyword: "",
+      options:[],
+    };
+  },
+  mounted(){
+      axios.get('/user').then(({data})=>{
+          console.log(data)
+      })
+  },
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    },
+    // handledbClick(row) {
+    //     console.log(row.id)
+    // }
+    getListData() {
+        // ?cb=1597311580379&keyword=&auditType=CONTRACT&operator=&createTimeFrom=&createTimeTo=&operation=&pageNo=1&pageSize=10&companyId=2703493711465512985
+      axios
+        .get("/system/audit/list",{
+            params:this.reqParams
+        })
+        .then((res) => {
+          console.log(res.data.result.result);
+          this.tableData = res.data.result.result
+        });
+    },
+  },
+  computed: {
+      reqParams() {
+          let createTimeFrom, createTimeTo
+          if(this.valuePicter) {
+              createTimeFrom = this.valuePicter[0]
+              createTimeTo = this.valuePicter[1]
+          }else {
+              createTimeFrom = ''
+              createTimeTo = ''
+          }
+          console.log(this.auditType)
+          return {
+             createTimeFrom,
+             createTimeTo,
+             operator:this.operator,
+             operation:this.operation, 
+             keyword:this.keyword, 
+             auditType:this.auditType,
+             companyId:'2703493711465512985',
+          }
+      }
+  },
 
-            }   
-        };
-    },
-    methods: {
-        handleSizeChange(val) {
-            console.log(`每页 ${val} 条`);
-        },
-        handleCurrentChange(val) {
-            console.log(`当前页: ${val}`);
-        },
-        // handledbClick(row) {
-        //     console.log(row.id)
-        // }
-    },
-    async created(){
-        const res = await axios('/system/audit/list?cb=1597311580379&keyword=&auditType=CONTRACT&operator=&createTimeFrom=&createTimeTo=&operation=&pageNo=1&pageSize=10&companyId=2703493711465512985')
-        console.log(res)
-        this.tableData = res.data.result.result
-        // .then(res=> {
-        //     this.tableData = res.data.result.result
-        // })
-    }
+  watch: {
+      reqParams() {
+          this.getListData()
+      }
+  },
+  created() {
+    this.getListData();
+  },
+  // async created(){
+  //     const res = await axios('/system/audit/list?cb=1597311580379&keyword=&auditType=CONTRACT&operator=&createTimeFrom=&createTimeTo=&operation=&pageNo=1&pageSize=10&companyId=2703493711465512985')
+  //     console.log(res)
+  //     this.tableData = res.data.result.result
+  // }
 };
-
 </script>
 <style scoped>
 .table-list {
-    margin: 0px 30px 0 30px;
+  margin: 0px 30px 0 30px;
+  text-align: left;
 }
 .pagination {
-    padding: 10px 24px;
-    display: flex;
-    justify-content: flex-end;
+  padding: 10px 24px;
+  display: flex;
+  justify-content: flex-end;
 }
 #search-module {
-    width: 100%;
-    height: 113px;
-    padding: 30px 0 30px 30px;
-    border-bottom: 1px solid rgb(228, 231, 237);
+  width: 100%;
+  height: 113px;
+  padding: 30px 0 30px 30px;
+  border-bottom: 1px solid rgb(228, 231, 237);
 }
 .filter {
-    display: flex;
-    justify-content: space-around;
-    flex-wrap: wrap;
-    padding-right: 200px;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  padding-right: 200px;
 }
 </style>
