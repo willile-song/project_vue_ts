61d22c0bfddc8c08e3921cbc7890095b46df91c1
Your Name <willile356428@163.com>
2020-08-18 08:41:31 +0800

    完善路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -43,7 +43,12 @@ const routes: Array<RouteConfig> = [
             }
         ]
     },
-    
+    {
+        path: "*",
+        name: 'notFound',
+        component: () => import('../views/notFound.vue'),
+    },
+
 ]
 
 const router = new VueRouter({
---
+++ src/views/notFound.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>页面不存在!!!</p>
+    </div>
+</template>
+
+
+<style scoped>
+    div {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: goldenrod;
+    }
+</style>
